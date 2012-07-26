e70d798879b054445b3e53addc9694545a0ef06f
tav <tav@espians.com>
2012-07-26 03:32:33 +0100

    app: added support for a basic config file.

--- app/planfile.go
+++ app/planfile.go
@@ -1,20 +1,52 @@
+// Public Domain (-) 2012 The Planfile App Authors.
+// See the Planfile App UNLICENSE file for details.
+
 package main
 
 import (
+	"amp/yaml"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+type Config struct {
+	Debug               bool
+	GoogleAnalyticsHost string
+	GoogleAnalyticsID   string
+	LocalDirectory      string
+	OAuthClientID       string
+	OAuthClientSecret   string
+	Repository          string
+	UseLocal            bool
+	TypekitID           string
+}
+
+var config = &Config{}
+
 func main() {
-	http.HandleFunc("/", hello)
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
+	cfg, err := yaml.ParseFile("config.yaml")
 	if err != nil {
-		log.Fatal("ListenAndServe:", err)
+		log.Fatal("ERROR: couldn't parse the config file: ", err)
 	}
+
+	cfg.LoadStruct(config)
+	if config.LocalDirectory != "" {
+		config.UseLocal = true
+	}
+
+	http.HandleFunc("/", hello)
+
+	err = http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatal("ERROR: couldn't bind to tcp socket: ", err)
+	}
+
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "hello, world!")
+	fmt.Fprintln(w, "hello, world!\n")
+	fmt.Fprintf(w, "%#v", config)
 }
